a1262e8886e4bf4569b5c3f9bf0855ad8a97876b
nazmulhasan598040@gmail.com <mdnazmulhasan@bracits.com>
2022-04-10 18:59:15 +0600

    product delete implementation is done

--- src/service/productServices.tsx
+++ src/service/productServices.tsx
@@ -6,3 +6,11 @@ const baseUrl: string = process.env.REACT_APP_BASE_URL || ''
 export const postProduct = (data: FormData) =>{
   return axios.post(`${baseUrl}products/`, data)
 }
+
+export const getProducts = () =>{
+  return axios.get(`${baseUrl}products/`)
+}
+
+export const deleteProduct = (id: number) =>{
+  return axios.delete(`${baseUrl}products/${id}`)
+}
